cd8a3251958d340c94ad459c2c43bf7e13249166
Allchan <85478918+a11chan@users.noreply.github.com>
2022-01-31 13:06:57 +0900

    Update js-clock.js
    after

--- js-clock.js
+++ js-clock.js
@@ -1,23 +1,24 @@
-const merry = document.querySelector(".js-clock");
-
 function getClock() {
-const christmas = new Date("2021, 12, 25");
-const date = new Date();
-const timeGap = christmas - date;
+  const xMas = new Date("2021, 12, 25");
+  let today = new Date();
+  let timesLeft = xMas - today;
+	let perDay = 1000*60*60*24
+	let perHour = 1000*60*60
+	let perMinute = 1000*60
 
-const xDay = Math.floor(timeGap / (1000 * 60 * 60 * 24));
-const xHours = Math.floor(
-(timeGap - xDay * 1000 * 60 * 60 * 24) / (1000 * 60 * 60)
-);
-const xMinutes = Math.floor((timeGap % (60 * 60 * 1000)) / (60 * 1000));
-const xSeconds = Math.floor((timeGap % (60 * 1000)) / 1000);
+    let daysLeft = Math.floor(timesLeft / perDay);
+    let hoursLeft = Math.floor((timesLeft % perDay) / perHour);
+    let minutesLeft = Math.floor((timesLeft % perHour) / perMinute );
+    let secondsLeft = Math.floor((timesLeft % perMinute) / 1000);
 
-const day = String(xDay).padStart(2, "0");
-const hours = String(xHours).padStart(2, "0");
-const minutes = String(xMinutes).padStart(2, "0");
-const seconds = String(xSeconds).padStart(2, "0");
+    let dayPrint = String(daysLeft).padStart(2, "0");
+    let hourPrint = String(hoursLeft).padStart(2, "0");
+    let minutePrint = String(minutesLeft).padStart(2, "0");
+    let secondPrint = String(secondsLeft).padStart(2, "0");
 
-merry.innerText = `${day}d ${hours}h ${minutes}m ${seconds}s`;
+    let merry = document.querySelector(".js-clock");
+    merry.innerText =
+        `${dayPrint}d ${hourPrint}h ${minutePrint}m ${secondPrint}s`;
 }
 
 getClock();
