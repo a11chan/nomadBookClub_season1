caa265c22809ad490805fe86353fd0f34b025a5b
Allchan <85478918+a11chan@users.noreply.github.com>
2022-02-16 16:13:22 +0900

    Update js-clock.js

--- js-clock.js
+++ js-clock.js
@@ -1,25 +1,35 @@
-function showClock() {
+function showTimer() {
+    let timesLeft = getTimesLeft();
+    let eachLeftTime = getEachLeftTime(timesLeft);
+    let timerNumbers = document.querySelector(".js-clock");
+    timerNumbers.innerText = getTimeString(eachLeftTime);
+}
+
+function getTimesLeft(){
     const xMas = new Date("2022, 12, 25");
     let today = new Date();
-    let timesLeft = xMas - today;
-    let day = hour*24
-    let hour = minute*60
+    return xMas - today
+};
+
+function getEachLeftTime (timesLeft) {
     let minute = 1000*60
+    let hour = minute*60
+    let day = hour*24
 
     let daysLeft = Math.floor(timesLeft / day);
     let hoursLeft = Math.floor((timesLeft % day) / hour);
     let minutesLeft = Math.floor((timesLeft % hour) / minute);
     let secondsLeft = Math.floor((timesLeft % minute) / 1000);
-
-    let dayPrint = String(daysLeft).padStart(2, "0");
-    let hourPrint = String(hoursLeft).padStart(2, "0");
-    let minutePrint = String(minutesLeft).padStart(2, "0");
-    let secondPrint = String(secondsLeft).padStart(2, "0");
-
-    let clockPrint = document.querySelector(".js-clock");
-    clockPrint.innerText =
-        `${dayPrint}d ${hourPrint}h ${minutePrint}m ${secondPrint}s`;
+    return {daysLeft, hoursLeft, minutesLeft, secondsLeft}
 }
 
-showClock();
-setInterval(showClock, 1000);
+function getTimeString ({daysLeft, hoursLeft, minutesLeft, secondsLeft}){
+    let dayString = String(daysLeft).padStart(2, "0");
+    let hourString = String(hoursLeft).padStart(2, "0");
+    let minuteString = String(minutesLeft).padStart(2, "0");
+    let secondString = String(secondsLeft).padStart(2, "0");    
+    return `${dayString}d ${hourString}h ${minuteString}m ${secondString}s`;
+};
+
+showTimer();
+setInterval(showTimer, 1000);
